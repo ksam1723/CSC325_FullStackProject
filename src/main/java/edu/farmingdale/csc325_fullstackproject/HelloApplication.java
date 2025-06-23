fb0e46c762a4c17db40e240b9adf3209aa5a204c
ksam1723 <kevinsam68@gmail.com>
2025-06-23 00:30:52 -0400

    Created menu bar

--- src/main/java/edu/farmingdale/csc325_fullstackproject/HelloApplication.java
+++ src/main/java/edu/farmingdale/csc325_fullstackproject/HelloApplication.java
@@ -3,18 +3,25 @@ package edu.farmingdale.csc325_fullstackproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage){
+        // Menu
+        MenuBar menubar = new MenuBar();
+        menubar.getMenus().addAll(
+                new Menu("File"),
+                new Menu("Edit"),
+                new Menu("Theme"),
+                new Menu("Help")
+        );
+
+
+
     }
 
     public static void main(String[] args) {
